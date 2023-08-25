f425fb4cc8593602cabf07c246d2a5d0b18d0265
mohammadshamimreza <mohammadshamimreza23393@gmail.com>
2023-08-25 20:26:08 +0600

    [done] course

--- src/app/module/course/course.service.ts
+++ src/app/module/course/course.service.ts
@@ -1,15 +1,49 @@
 import { Course } from '@prisma/client';
 import prisma from '../../../shared/prisma';
 
-const insertIntoDB = async (data: Course): Promise<Course> => {
-  const result = await prisma.course.create({
-    data,
-    include: {
-      preRequisite: true,
-      preRequisiteFor: true,
-    },
+const insertIntoDB = async (data: any): Promise<Course> => {
+  const { preRequisiteCourses, ...courseData } = data;
+
+  const newCourse = await prisma.$transaction(async transectionClient => {
+    const result = await transectionClient.course.create({
+      data: courseData,
+    });
+
+    if (preRequisiteCourses && preRequisiteCourses.length > 0) {
+      for (let i = 0; i < preRequisiteCourses.length; i++) {
+        const createPreRequisite =
+          await transectionClient.courseToPrerequisite.create({
+            data: {
+              courseId: result.id,
+              preRequisiteId: preRequisiteCourses[i].courseId,
+            },
+          });
+        console.log(createPreRequisite);
+      }
+    }
+    return result;
   });
-  return result;
+
+  if (newCourse) {
+    const responseData = await prisma.course.findUnique({
+      where: {
+        id: newCourse.id,
+      },
+      include: {
+        preRequisite: {
+          include: {
+            preRequisite: true,
+          },
+        },
+        preRequisiteFor: {
+          include: {
+            course: true,
+          },
+        },
+      },
+    });
+    return responseData;
+  }
 };
 
 export const CourseService = {
